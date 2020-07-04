3ea41fec9b0e956e104181ce71fd27f1f0c70550
Rohini Rao <67069853+Romcegsr@users.noreply.github.com>
2020-07-04 12:15:32 +0100

    Working Javascript WebdriverIo project

---
+++ test/element.js
@@ -0,0 +1,19 @@
+describe("interaction with web element", function(){
+
+    it("Enter Username, User password and click Login", function(){
+        browser.url('/')
+        const userName = $('#user-name');
+        const userPassword = $('#password');
+        const loginButton = $('#login_button_container > div > form > input.btn_action');    
+
+        userName.setValue('standard_user');
+        userPassword.setValue('secret_sauce');
+        browser.pause(5000);
+
+        loginButton.click();
+
+        
+    });
+    
+
+});
